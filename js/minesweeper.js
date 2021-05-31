0225fb455a04f6d7676abb40c34eeefa1a831be0
Tyler <44188028+twit96@users.noreply.github.com>
2021-05-30 21:33:36 -0500

    Responsive Layout
    added scoreboard with dummy elements
    
    in preparation for variable grid sizes, added js to set the dimensions of the game layout based on the current screen size and grid size

---
+++ js/minesweeper.js
@@ -0,0 +1,163 @@
+var difficulty = 'easy';
+var num_rows;
+var num_cols;
+var num_mines;
+var grid;
+
+var scoreboard = document.getElementById("scoreboard");
+var board = document.getElementById('board');  // game board
+
+function setPresets(difficulty) {
+  var presets = {
+    // 'difficulty': [rows, cols, mines]
+    'easy':          [10, 10, 10],
+    'intermediate':  [16, 16, 40],
+    'expert':        [30, 16, 99]
+  }
+  var choice = presets[difficulty];
+  // set vars
+  num_rows = choice[0];
+  num_cols = choice[1];
+  num_mines = choice[2];
+  grid = [];
+  // update board layout
+  board.style.gridTemplateColumns = 'repeat(' + num_cols + ', 1fr)';
+  board.style.gridTemplateRows = 'repeat(' + num_rows + ', 1fr)';
+}
+
+
+function generateBoard() {
+  for (r=0; r<num_rows; r++) {
+    grid[r] = [];
+    for (c=0; c<num_cols; c++) {
+      grid[r][c] = 0;
+    }
+  }
+}
+
+
+function generateMines() {
+  var rand_row;
+  var rand_col;
+  for (m=0; m<num_mines; m++) {
+    do {
+      rand_row = Math.floor(Math.random() * num_rows);
+      rand_col = Math.floor(Math.random() * num_cols);
+    } while (grid[rand_row][rand_col] == "M")
+    grid[rand_row][rand_col] = "M";
+  }
+}
+
+
+function generateTileNumbers() {
+  for (r=0; r<num_rows; r++) {
+    for (c=0; c<num_cols; c++) {
+      if (grid[r][c] != "M") {
+        checkAdjacentTiles(r,c);
+      }
+    }
+  }
+}
+
+
+function checkAdjacentTiles(row, col) {
+  var mines = 0;
+  for (var r=row-1; r<=row+1; r++) {
+    for (var c=col-1; c<=col+1; c++) {
+      try {
+        if (grid[r][c] == "M") { mines++; }
+      } catch {
+        continue;
+      }
+    }
+  }
+  grid[row][col] = mines;
+}
+
+
+function clearBoard() {
+  while (board.firstChild) { board.removeChild(board.firstChild); }
+}
+
+
+function configureBoard() {
+  clearBoard();
+  for (r=0; r<num_rows; r++) {
+    for (c=0; c<num_cols; c++) {
+      var cell = document.createElement('span');
+      var cell_id = 'c-' + r.toString() + '-' + c.toString();
+      cell.setAttribute('id', cell_id);
+      cell.classList.add('cell');
+      cell.innerHTML = grid[r][c];
+      cell.setAttribute('onclick', 'clickCell(this, this.id)');
+
+      cell.classList.add('num' + grid[r][c]);
+
+      var cover = document.createElement('span');
+      cover.classList.add('cover');
+
+      cell.insertAdjacentElement('beforeend', cover);
+      board.insertAdjacentElement('beforeend', cell);
+    }
+  }
+}
+
+
+function configureGame(difficulty) {
+  setPresets(difficulty);
+  generateBoard();
+  generateMines();
+  generateTileNumbers();
+  // console.table(grid);
+  configureBoard();
+}
+configureGame(difficulty);
+// setInterval(configureGame, 2000);
+
+
+function clickCell(cell, cell_id) {
+
+  // Update Cell
+  var cell_cover = cell.querySelector('.cover');
+  if (cell_cover) {
+    cell.removeChild(cell_cover);
+    cell.onclick = null;
+    cell.classList.add('clicked');
+  }
+
+  // Check Tile Value
+  var val = cell.innerHTML[0];
+
+  // hit mine
+  if (val == 'M') { return; }
+
+  // hit nonzero number
+  if (val != 0) { return; }
+
+  // hit 0 - recursion
+  else {
+    var split_id = cell_id.split('-');
+    var row = parseInt(split_id[1]);
+    var col = parseInt(split_id[2]);
+
+    var r = Math.max(row-1, 0);
+    var r_hi = Math.min(row+1, num_rows-1);
+    var c;
+    var c_hi;
+
+
+    for (r; r<r_hi+1; r++) {
+      c = Math.max(col-1, 0);
+      c_hi = Math.min(col+1, num_cols-1);
+      for (c; c<=c_hi; c++) {
+
+        var adj_cell_id = 'c-' + String(r) + '-' + String(c);
+        var adj_cell = document.getElementById(adj_cell_id);
+
+        if ((adj_cell_id != cell_id) && (adj_cell.querySelector('.cover') != null)) {
+          clickCell(adj_cell, adj_cell_id);
+        }
+      }
+    }
+  }
+}
---
+++ js/styles.js
@@ -0,0 +1,83 @@
+// var body_width;
+// var body_height;
+// function setDimensions() {
+//   body_width = window.innerWidth;
+//   body_height = window.innerHeight;
+//
+//   var scoreboard_height = scoreboard.offsetHeight;
+//   var board_height = body_height - scoreboard_height;
+//
+//   var view_min = Math.min(board_height, body_width);
+//   scoreboard.style.width = view_min + "px";
+//   board.style.height = view_min + "px";
+//   board.style.width = view_min + "px";
+//   var cell_max = Math.max(num_rows, num_cols);
+//
+//   var cell_dimensions = view_min / cell_max;
+//   var cell_font_size = (cell_dimensions / 2) + "px";
+//
+//   var elements = document.getElementsByClassName('cell');
+//   for (var i = 0; i < elements.length; i++) {
+//     elements[i].style.width = cell_dimensions;
+//     elements[i].style.height = cell_dimensions;
+//     elements[i].style.fontSize = cell_font_size;
+//   }
+// }
+
+
+var body = document.querySelector('body');
+var body_width;
+var body_height;
+function setDimensions() {
+
+  body_width = window.innerWidth;
+  body_height = window.innerHeight;
+
+  var scoreboard_height = scoreboard.offsetHeight;
+  var board_height = body_height - scoreboard_height;
+  var view_min = Math.min(board_height, body_width);
+
+  var body_padding = Math.floor(0.025 * view_min);
+  board_height -= (3 * body_padding);
+  body_width -= (2 * body_padding);
+  view_min = Math.min(board_height, body_width);
+
+  var cell_max = Math.max(num_rows, num_cols);
+  var cell_dimensions = view_min / cell_max;
+  var cell_font_size = (cell_dimensions / 2) + "px";
+
+  body.style.padding = body_padding + "px";
+  scoreboard.style.marginBottom = body_padding + "px";
+  scoreboard.style.width = view_min + "px";
+  board.style.height = view_min + "px";
+  board.style.width = view_min + "px";
+
+  if (
+    (difficulty) &&
+    (difficulty == 'expert') &&
+    (window.innerWidth <= 1000)
+  ) {
+    board.style.height = '90vh';
+  }
+
+  var elements = document.getElementsByClassName('cell');
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].style.width = cell_dimensions;
+    elements[i].style.height = cell_dimensions;
+    elements[i].style.fontSize = cell_font_size;
+  }
+
+
+}
+
+
+window.addEventListener('resize', function() {
+  if (
+    (body_width != window.innerWidth) ||
+    (body_height != window.innerHeight)
+  ) {
+    setDimensions();
+  }
+});
+
+setDimensions();
