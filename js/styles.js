ef679833a4359f79673dfec7d21cb9b03e8b7680
Tyler <44188028+twit96@users.noreply.github.com>
2021-06-01 21:51:54 -0500

    setDimensions tweak
    update overall board height configuration to account for expert mode difficulty where num_rows != num_cols.

--- js/styles.js
+++ js/styles.js
@@ -12,35 +12,28 @@ function setDimensions() {
 
   var scoreboard_height = scoreboard.offsetHeight;
   var settings_height = settings.offsetHeight;
-  var board_height = body_height - scoreboard_height - settings_height;
-  var view_min = Math.min(board_height, body_width);
+  var available_height = body_height - scoreboard_height - settings_height;
 
+  var view_min = Math.min(available_height, body_width);
   var body_padding = Math.floor(0.025 * view_min);
-  board_height -= (4 * body_padding);
-  body_width -= (2 * body_padding);
-  view_min = Math.min(board_height, body_width);
 
-  var cell_max = Math.max(num_rows, num_cols);
-  var cell_dimensions = view_min / cell_max;
-  var cell_font_size = (cell_dimensions / 2) + "px";
+  available_height -= (4 * body_padding);
+  var available_width = body_width - (2 * body_padding);
+  view_min = Math.min(available_height, available_width);
+
+  var cell_dimensions = Math.min(available_width/num_cols, available_height/num_rows);
+  var cell_font_size = (cell_dimensions/2) + "px";
+
+  var board_width = (cell_dimensions * num_cols);
+  var board_height = (cell_dimensions * num_rows);
 
   body.style.padding = body_padding + "px";
   scoreboard.style.marginBottom = body_padding + "px";
   scoreboard.style.width = view_min + "px";
-  board.style.height = view_min + "px";
-  board.style.width = view_min + "px";
+  board.style.width = board_width + 'px';
+  board.style.height = board_height + 'px';
   settings.style.width = view_min + "px";
   settings.style.marginTop = body_padding + "px";
-
-  if (
-    (difficulty) &&
-    (difficulty == 'expert') &&
-    (window.innerWidth <= 700)
-  ) {
-    var view_max = Math.max(board_height, body_width) - (4 * body_padding);
-    board.style.height = view_max + 'px';
-    cell_font_size = (cell_dimensions / 8) * 7 + "px";
-  }
 
   var cells = document.getElementsByClassName('cell');
   for (var i = 0; i < cells.length; i++) {
